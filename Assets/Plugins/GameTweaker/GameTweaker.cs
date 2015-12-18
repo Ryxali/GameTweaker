b7b1206d8d94f1e7df4b2f485e69d70668ae8e89
Ryxali <ryxali@gmail.com>
2015-12-18 01:53:41 +0100

    Added prefab support, revert still works funky

--- Assets/Plugins/GameTweaker/GameTweaker.cs
+++ Assets/Plugins/GameTweaker/GameTweaker.cs
@@ -42,6 +42,7 @@ public class GameTweaker : EditorWindow {
     {
         // Class type
         public Type type;
+        public bool hasPrefab;
         // Tweakable fields
         public FieldInfo[] sharedFields;
         public FieldInfo[] instancedFields;
@@ -70,7 +71,7 @@ public class GameTweaker : EditorWindow {
         tweakables = new List<TweakableClass>();
         foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
         {
-            if(assembly.GetName().Name == "Assembly-CSharp")
+            if (assembly.GetName().Name == "Assembly-CSharp-firstpass" || assembly.GetName().Name == "Assembly-CSharp")
                 foreach (Type type in assembly.GetTypes())
                 {
                     List<FieldInfo> sharedFields = new List<FieldInfo>();
@@ -107,7 +108,25 @@ public class GameTweaker : EditorWindow {
                         c.type = type;
                         c.sharedFields = sharedFields.ToArray();
                         c.instancedFields = instancedFields.ToArray();
-                        c.objects = FindObjectsOfType(type);
+                        c.objects = Resources.FindObjectsOfTypeAll(type);
+                        UnityEngine.Object obj;
+                        if(c.objects.Length > 0 && (obj = PrefabUtility.GetPrefabParent(c.objects[0])) != null) {
+                            c.hasPrefab = true;
+                            for (int i = 0; i < c.objects.Length; i++)
+                            {
+                                if (c.objects[i] == obj)
+                                {
+                                    var tObj = c.objects[0];
+                                    c.objects[0] = c.objects[i];
+                                    c.objects[i] = tObj;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            c.hasPrefab = false;
+                        }
                         tweakables.Add(c);
                     }
                 }
@@ -128,10 +147,12 @@ public class GameTweaker : EditorWindow {
                     EditorGUI.indentLevel++;
                     EditorGUILayout.LabelField("Shared Settings", EditorStyles.boldLabel);
                     EditorGUI.indentLevel++;
+                    
                     SerializedObject o = new SerializedObject(c.objects[0]);
                     List<SerializedProperty> props = new List<SerializedProperty>();
                     foreach (FieldInfo field in c.sharedFields)
                     {
+                        
                         SerializedProperty p = o.FindProperty(field.Name);
                         if (p == null)
                         {
@@ -148,18 +169,20 @@ public class GameTweaker : EditorWindow {
                     o.ApplyModifiedProperties();
                     
                     EditorGUI.indentLevel--;
-                    
+
                     
                     foreach (UnityEngine.Object obj in c.objects)
                     {
                         EditorGUILayout.LabelField(obj.name, EditorStyles.boldLabel);
                         EditorGUI.indentLevel++;
                         SerializedObject instObj = new SerializedObject(obj);
-                        foreach (SerializedProperty p in props)
+                        foreach (SerializedProperty p in props) // copy shared props
                         {
                             instObj.CopyFromSerializedProperty(p);
                         }
                         instObj.ApplyModifiedPropertiesWithoutUndo();
+
+                        GUI.changed = false;
                         foreach (FieldInfo field in c.instancedFields)
                         {
                             SerializedProperty prop = instObj.FindProperty(field.Name);
@@ -168,8 +191,41 @@ public class GameTweaker : EditorWindow {
                                 Debug.LogWarning("non-properties aren't supported yet. The type: (" + field.FieldType + ") of \"" + c.type + "." + field.Name + "\" is currently illegal.");
                             } else
                                 PropertyField(prop);
+                            
                         }
+
                         instObj.ApplyModifiedProperties();
+                        UnityEngine.Object objParent;
+                        if (funkyProf != null && prefabFunky && (objParent = PrefabUtility.GetPrefabParent(obj)) != null && objParent != obj)
+                        {
+                            /*var mods = PrefabUtility.GetPropertyModifications(obj);
+                            PropertyModification mod = new PropertyModification();
+                            var proppy = instObj.FindProperty(funkyProf);
+                            var proppy2 = new SerializedObject(PrefabUtility.GetPrefabParent(obj)).FindProperty("funkyProf");
+                            if (!SerializedProperty.EqualContents(proppy, proppy2))
+                            {
+                                PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
+                                
+                            }
+                            else
+                            {
+                                proppy.prefabOverride = false;
+                                instObj.ApplyModifiedProperties();
+                            }*/
+
+                            PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
+                            /*
+                            PropertyModification[] mods2 = new PropertyModification[mods.Length + 1];
+                            mods.CopyTo(mods2, 1);
+                            mods[0] = mod;
+                            PrefabUtility.SetPropertyModifications(obj, mods);*/
+                            Debug.Log("HUNKY DORY");
+                            funkyProf = null;
+                            prefabFunky = false;
+                            //
+                        }
+                        GUI.changed = false;
+                        
                         EditorGUI.indentLevel--;
                     }
                     
@@ -187,6 +243,11 @@ public class GameTweaker : EditorWindow {
         }
         
     }
+
+    
+
+    static bool prefabFunky = false;
+    static string funkyProf = null;
     /// <summary>
     /// helper function to automatically handle arrays
     /// </summary>
@@ -197,10 +258,46 @@ public class GameTweaker : EditorWindow {
         if (prop.isArray)
         {
             DrawArrayProperty(prop);
+
+            
         }
         else
         {
+            funkyProf = prop.name;
             EditorGUILayout.PropertyField(prop);
+            if (!GUI.changed)
+                funkyProf = null;
+            else
+            {
+                if (prop.isInstantiatedPrefab)
+                {
+                    var preProp = new SerializedObject(PrefabUtility.GetPrefabParent(prop.serializedObject.targetObject)).FindProperty(prop.name);
+                    Debug.Log(prop.name + " ||| " + preProp.name);
+                    if (SerializedProperty.EqualContents(prop, preProp))
+                    {
+                        prop.prefabOverride = false;
+                    }
+                    else
+                    {
+                        prefabFunky = true;
+                    }
+                    
+                }
+                
+                
+            }
+            if (prop.isInstantiatedPrefab)
+                {
+                    var preProp = new SerializedObject(PrefabUtility.GetPrefabParent(prop.serializedObject.targetObject)).FindProperty(prop.name);
+                    Debug.Log(prop.name + " ||| " + preProp.name);
+                    if (SerializedProperty.EqualContents(prop, preProp))
+                    {
+                        prop.prefabOverride = false;
+                    }
+                    
+                }
+                
+            GUI.changed = false;
         }
     }
 
