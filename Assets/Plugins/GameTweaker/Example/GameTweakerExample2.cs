3a9d433703ad96f5251070eefbc4812c213d7961
Ryxali <ryxali@gmail.com>
2015-12-18 01:53:23 +0100

    Unity 5.3 engage!

--- Assets/Plugins/GameTweaker/Example/GameTweakerExample2.cs
+++ Assets/Plugins/GameTweaker/Example/GameTweakerExample2.cs
@@ -5,7 +5,7 @@ public class GameTweakerExample2 : MonoBehaviour {
     [TweakableField]
     public int foo;
     [TweakableField]
-    public int[][] foo2;
+    public int[] foo2;
     [TweakableField(true)]
     public int sharedFoo;
 	// Use this for initialization
